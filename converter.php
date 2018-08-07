389635fe9c53d3251280bd451a72d550f2538c42
lorjdim1985 <42154811+lorjdim1985@users.noreply.github.com>
2018-08-07 21:29:29 +0200

    Add round to summaries

--- converter.php
+++ converter.php
@@ -82,7 +82,7 @@ class EmailToCSVConverter {
 
         $this->result_to_screen();
         echo 'Max memory usage: ' . memory_get_peak_usage(TRUE)/1024/1024 . 'MB' . '<br />';
-        echo 'Script Execution time: ' . ($time_end - $time_start) . ' seconds, ' . $execution_time . ' minutes';
+        echo 'Script Execution time: ' . round(($time_end - $time_start), 4) . ' seconds, ' . round($execution_time, 4) . ' minutes';
         
     }
 
